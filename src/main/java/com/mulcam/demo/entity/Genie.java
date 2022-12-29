3d1e5a14a7deaf55d5eb353555ff318bb6bd9700
Cheolkie Kim <ckiekim@naver.com>
2022-12-29 17:40:50 +0900

    Crawling

---
+++ src/main/java/com/mulcam/demo/entity/Genie.java
@@ -0,0 +1,52 @@
+package com.mulcam.demo.entity;
+
+public class Genie {
+	private int rank;
+	private String image;
+	private String title;
+	private String artist;
+	private String album;
+	public Genie() { }
+	public Genie(int rank, String image, String title, String artist, String album) {
+		this.rank = rank;
+		this.image = image;
+		this.title = title;
+		this.artist = artist;
+		this.album = album;
+	}
+	@Override
+	public String toString() {
+		return "Genie [rank=" + rank + ", image=" + image + ", title=" + title + ", artist=" + artist + ", album="
+				+ album + "]";
+	}
+	public int getRank() {
+		return rank;
+	}
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public String getAlbum() {
+		return album;
+	}
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+}
--- src/main/java/com/mulcam/demo/service/Crawling.java
+++ src/main/java/com/mulcam/demo/service/Crawling.java
@@ -9,6 +9,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import com.mulcam.demo.entity.Genie;
 import com.mulcam.demo.entity.Interpark;
 
 public class Crawling {
@@ -17,13 +18,24 @@ public class Crawling {
 		bestSeller();
 	}
 	
+	public static List<Genie> genieChart() throws IOException {
+		
+		
+		
+		List<Genie> list = new ArrayList<>();
+		
+		
+		
+		return list;
+	}
+	
 	public static List<Interpark> bestSeller() throws IOException {
 		String urlBase = "http://book.interpark.com";
 		String urlSub = "/display/collectlist.do?_method=bestsellerHourNew&bookblockname=b_gnb&booklinkname=%BA%A3%BD%BA%C6%AE%C1%B8&bid1=bgnb_mn&bid2=LiveRanking&bid3=main&bid4=001";
 		String url = urlBase + urlSub;
 		Document doc = Jsoup.connect(url).get();
 		Elements lis = doc.select(".listItem.singleType");
-		System.out.println(lis.size());
+//		System.out.println(lis.size());
 		
 		List<Interpark> list = new ArrayList<>();
 		for (Element li: lis) {
@@ -37,7 +49,7 @@ public class Crawling {
 			String rank_ = "";
 			for (Element span: spans) {
 				String classes = span.attr("class").strip();
-				//System.out.println(classes);
+//				System.out.println(classes);
 				rank_ += classes.substring(classes.length() - 1);
 			}
 			int rank = Integer.parseInt(rank_);
@@ -45,7 +57,7 @@ public class Crawling {
 			Interpark ip = new Interpark(rank, src, title, author, company, price);
 			list.add(ip);
 		}
-		//list.forEach(x -> System.out.println(x));
+//		list.forEach(x -> System.out.println(x));
 		return list;
 	}
 	
