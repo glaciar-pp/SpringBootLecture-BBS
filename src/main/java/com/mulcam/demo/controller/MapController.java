91f6f3b4befaea0cccdc6cfd90a5eecd1630f40c
Cheolkie Kim <ckiekim@naver.com>
2023-01-03 17:39:23 +0900

    Naver API

--- src/main/java/com/mulcam/demo/controller/MapController.java
+++ src/main/java/com/mulcam/demo/controller/MapController.java
@@ -1,5 +1,8 @@
 package com.mulcam.demo.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,7 +28,7 @@ public class MapController {
 	}
 	
 	@PostMapping("/staticMap")
-	public String staticMap(StaticMap map, Model model) {
+	public String staticMap(StaticMap map, Model model) throws UnsupportedEncodingException {
 		String url = "https://naveropenapi.apigw.ntruss.com/map-static/v2/raster"
 					+ "?w=" + map.getWidth()
 					+ "&h=" + map.getHeight()
@@ -37,6 +40,14 @@ public class MapController {
 					+ "&lang=" + map.getLang()
 					+ "&X-NCP-APIGW-API-KEY-ID=" + accessId
 					+ "&X-NCP-APIGW-API-KEY=" + secretKey;
+		
+		String marker = "type:d|size:mid|pos:127.0724 37.5383";
+		marker = URLEncoder.encode(marker, "utf-8");
+		url += "&markers=" + marker;
+
+		marker = "type:t|size:tiny|pos:127.0824 37.5383|label:광진구청|color:red";
+		marker = URLEncoder.encode(marker, "utf-8");
+		url += "&markers=" + marker;
 		
 		model.addAttribute("url", url);
 		return "map/staticResult";
