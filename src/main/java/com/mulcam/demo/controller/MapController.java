9e76c4cfcdeda67cb97b8e990dfbc358d000ce0d
Cheolkie Kim <ckiekim@naver.com>
2023-01-04 10:34:54 +0900

    Road API

--- src/main/java/com/mulcam/demo/controller/MapController.java
+++ src/main/java/com/mulcam/demo/controller/MapController.java
@@ -1,14 +1,22 @@
 package com.mulcam.demo.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
 import java.net.URLEncoder;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mulcam.demo.entity.StaticMap;
 
@@ -16,11 +24,9 @@ import com.mulcam.demo.entity.StaticMap;
 @RequestMapping("/map")
 public class MapController {
 
-	@Value("${naver.accessId}")
-	private String accessId;
-	
-	@Value("${naver.secretKey}")
-	private String secretKey;
+	@Value("${naver.accessId}") private String accessId;
+	@Value("${naver.secretKey}") private String secretKey;
+	@Value("${roadAddrKey}") private String roadAddrKey;
 	
 	@GetMapping("/staticMap")
 	public String staticForm() {
@@ -51,6 +57,40 @@ public class MapController {
 		
 		model.addAttribute("url", url);
 		return "map/staticResult";
+	}
+	
+	@ResponseBody
+	@GetMapping("/roadAddr/{keyword}")
+	public String roadAddr(@PathVariable String keyword) throws Exception {
+		int currentPage = 1;
+		int countPerPage = 10;
+		String resultType = "json";
+		keyword = URLEncoder.encode(keyword, "utf-8");
+		String apiUrl = "https://business.juso.go.kr/addrlink/addrLinkApi.do"
+						+ "?confmKey=" + roadAddrKey
+						+ "&currentPage=" + currentPage
+						+ "&countPerPage=" + countPerPage
+						+ "&keyword=" + keyword
+						+ "&resultType=" + resultType;
+		
+		URL url = new URL(apiUrl);
+		BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(), "utf-8"));
+		StringBuffer sb = new StringBuffer();
+		String line = null;
+		
+		while((line = br.readLine()) != null)
+			sb.append(line);
+		br.close();
+		
+		// JSON 데이터에서 원하는 값 추출하기
+		JSONParser parser = new JSONParser();
+		JSONObject object = (JSONObject) parser.parse(sb.toString());
+		JSONObject results = (JSONObject) object.get("results");
+		JSONArray juso = (JSONArray) results.get("juso");
+		JSONObject jusoItem = (JSONObject) juso.get(0);
+		String roadAddr = (String) jusoItem.get("roadAddr");
+		
+		return sb.toString() + "<br>" + roadAddr;
 	}
 	
 }
---
+++ src/main/java/com/mulcam/demo/service/RoadAddr.java
@@ -0,0 +1,47 @@
+package com.mulcam.demo.service;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.beans.factory.annotation.Value;
+
+public class RoadAddr {
+	@Value("${roadAddrKey}") private String roadAddrKey;
+	
+	public String getAddr(String keyword) throws Exception {
+		int currentPage = 1;
+		int countPerPage = 10;
+		String resultType = "json";
+		keyword = URLEncoder.encode(keyword, "utf-8");
+		String apiUrl = "https://business.juso.go.kr/addrlink/addrLinkApi.do"
+						+ "?confmKey=" + roadAddrKey
+						+ "&currentPage=" + currentPage
+						+ "&countPerPage=" + countPerPage
+						+ "&keyword=" + keyword
+						+ "&resultType=" + resultType;
+		
+		URL url = new URL(apiUrl);
+		BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(), "utf-8"));
+		StringBuffer sb = new StringBuffer();
+		String line = null;
+		
+		while((line = br.readLine()) != null)
+			sb.append(line);
+		br.close();
+		
+		// JSON 데이터에서 원하는 값 추출하기
+		JSONParser parser = new JSONParser();
+		JSONObject object = (JSONObject) parser.parse(sb.toString());
+		JSONObject results = (JSONObject) object.get("results");
+		JSONArray juso = (JSONArray) results.get("juso");
+		JSONObject jusoItem = (JSONObject) juso.get(0);
+		String roadAddr = (String) jusoItem.get("roadAddr");
+		
+		return sb.toString() + "<br>" + roadAddr;
+	}
+}
